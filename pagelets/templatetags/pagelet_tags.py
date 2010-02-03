442898a0b39cc0d861463790e32743a9dc76983b
glassresistor <glassresistor@a7d902ee-6656-11de-ac5a-e998ac4b807c>
2010-02-03 16:33:36 +0000

    Added context to template tags

--- pagelets/templatetags/pagelet_tags.py
+++ pagelets/templatetags/pagelet_tags.py
@@ -5,7 +5,7 @@ from django.conf import settings
 from django.template.loader import render_to_string
 from django.contrib.auth.models import User
 from django.utils.encoding import smart_str
-from django.template import Node, NodeList, Variable, Library
+from django.template import Node, NodeList, Variable, Library, Context
 from django.template import TemplateSyntaxError, VariableDoesNotExist
 from django.core.urlresolvers import reverse
 
@@ -19,7 +19,7 @@ def render_pagelet(context, pagelet):
     Renders the named pagelet in the calling template.
     """
     # don't modify the parent context
-    context = {'request': context['request'], 'perms': context['perms']}
+    context = Context({'request': context['request'], 'perms': context['perms']})
     if isinstance(pagelet, basestring):
         # add the slug separately because we need it in the template even
         # if this pagelet doesn't exist
@@ -45,7 +45,7 @@ def pagelink_ifexists(context, page, link_text):
     Renders a link to the given page in the calling template.
     """
     # don't modify the parent context
-    context = {}
+    context = Context({})
     if isinstance(page, basestring):
         # add the slug separately because we need it in the template even
         # if this page doesn't exist
