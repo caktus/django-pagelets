962fa447343f994fc889da6050c1e14cd7b38c7c
Colin Copeland <copelco@caktusgroup.com>
2010-09-09 10:45:33 -0700

    update pagelet.js to work with dynamically added pagelets in page admin

--- pagelets/media/js/pagelets.js
+++ pagelets/media/js/pagelets.js
@@ -1,36 +1,38 @@
-jQuery(function() {
+function setEditor(popup) {
+    var value = popup.find('>:selected').val();
+    var parent_form = popup.parents('form');
+    var content_field_name = popup.attr('name').replace(/type$/, 'content');
+    var content_field = parent_form.find('textarea[name=' + content_field_name + ']');
+    if (value == 'wymeditor') {
+        content_field.wymeditor({
+            updateSelector: "input:submit",
+            updateEvent: "click",
+        });
+    } else {
+        jQuery.each(WYMeditor.INSTANCES, function() {
+          if(this._element.attr('name') == content_field.attr('name')){
+              this.update();
+              $(this._box).remove();
+              $(this._element).show();
+              $(this._options.updateSelector).unbind(this._options.updateEvent);
+              delete this;
+          }
+        });
+        content_field.siblings('div.wym_box').remove();
+        content_field.css('display', 'inline');
+    }
+}
 
-    function setEditor(popup) {
-        var value = popup.find('>:selected').val();
-        var parent_form = popup.parents('form');
-        var content_field_name = popup.attr('name').replace(/type$/, 'content');
-        var content_field = parent_form.find('textarea[name=' + content_field_name + ']');
-        if (value == 'wymeditor') {
-            content_field.wymeditor({
-                updateSelector: "input:submit",
-                updateEvent: "click",
-            });
-        } else {
-            jQuery.each(WYMeditor.INSTANCES, function() {
-              if(this._element.attr('name') == content_field.attr('name')){
-                  this.update();
-                  $(this._box).remove();
-                  $(this._element).show();
-                  $(this._options.updateSelector).unbind(this._options.updateEvent);
-                  delete this;
-              }
-            });
-            content_field.siblings('div.wym_box').remove();
-            content_field.css('display', 'inline');
+jQuery(function() {
+    function install_editor(popup) {
+        var empty_form = $(popup).parents('div.empty-form');
+        if (empty_form.length == 0) {
+            setEditor($(popup));
         }
     }
-    jQuery('#pagelet-form select[name=type],' +
-           'body.pagelets-pagelet.change-form select[name=type],' +
-           'body.pagelets-page.change-form select[name$=type]').change(function(event) {
-        var popup = $(this);
-        setEditor(popup);
-    }).each(function (i, elem) {
-        setEditor($(elem));
+    $('select[name$=type]').live('change', function() {
+        install_editor($(this));
+    }).each(function (i) {
+        install_editor($(this));
     });
-    
 });
