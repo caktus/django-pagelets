478800999ecb5a3e9c26b1ffb11a1ebdd6274b86
mlavin <markdlavin@gmail.com>
2010-09-11 17:55:58 -0400

    Add tests for Page validators.

--- pagelets/tests.py
+++ pagelets/tests.py
@@ -3,6 +3,7 @@ from django.template import compile_string, TemplateSyntaxError, StringOrigin
 from django.template.context import Context
 from django.contrib.auth.models import User, Permission
 from django.core.urlresolvers import reverse
+from django.core.exceptions import ValidationError
 
 from pagelets.models import Page, Pagelet
 
@@ -85,4 +86,62 @@ class PageletsTest(TestCase):
         url = reverse('view_page', kwargs={'page_slug': page.slug})
         response = self.c.get(url)
         self.assertContains(response, pagelet.content)
+
+
+class PageTestCase(TestCase):
+    def setUp(self):
+        self.editor = User.objects.create_user(
+            username='editor',
+            email='editor@example.com',
+            password='abc123',
+        )
+        change_pagelet = Permission.objects.get(
+            codename='change_pagelet', 
+            content_type__app_label='pagelets', 
+            content_type__model='pagelet',
+        )
+        add_pagelet = Permission.objects.get(
+            codename='add_pagelet', 
+            content_type__app_label='pagelets', 
+            content_type__model='pagelet',
+        )
+        self.editor.user_permissions.add(change_pagelet)
+        self.editor.user_permissions.add(add_pagelet)
+
+    def testNoLeadingSlash(self):
+        "Don't allow a leading slash in the page slug."
+        test_page = Page(
+            title='New Page', 
+            slug='/new-page', 
+            created_by=self.editor, 
+            modified_by=self.editor,
+        )
+        self.assertRaises(ValidationError, test_page.full_clean)
+
+    def testNoTrailingSlash(self):
+        "Don't allow a trailing slash in the page slug."
+        test_page = Page(
+            title='New Page', 
+            slug='new-page/', 
+            created_by=self.editor, 
+            modified_by=self.editor,
+        )
+        self.assertRaises(ValidationError, test_page.full_clean)
+
+    def testRFC3986Characters(self):
+        """
+        Page slug should only contain characters defined in 
+        RFC 3986: http://www.ietf.org/rfc/rfc3986.txt
+        along with additional slashes.
+        """
+        test_page = Page(
+            title='New Page', 
+            slug='a/A/1/./~/-/_', 
+            created_by=self.editor, 
+            modified_by=self.editor,
+        )
+        try:
+            test_page.full_clean()
+        except ValidationError, e:
+            self.fail(str(e))
 
