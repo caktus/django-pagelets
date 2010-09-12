4e798e6cfe6c09d738859750f2c7a716415d9e65
Colin Copeland <copelco@caktusgroup.com>
2010-09-11 17:23:45 -0700

    set zip_safe=False; move classifiers into setup as list;

--- setup.py
+++ setup.py
@@ -1,20 +1,8 @@
-import os
 from setuptools import setup, find_packages
-from pagelets import __version__
-
-classifiers = """
-Topic :: Internet :: WWW/HTTP :: Dynamic Content
-Intended Audience :: Developers
-License :: OSI Approved :: BSD License
-Programming Language :: Python
-Topic :: Software Development :: Libraries :: Python Modules
-Development Status :: 4 - Beta
-Operating System :: OS Independent
-"""
 
 setup(
     name='django-pagelets',
-    version=__version__,
+    version=__import__('pagelets').__version__,
     author='Caktus Consulting Group',
     author_email='solutions@caktusgroup.com',
     packages=find_packages(exclude=['sample_project']),
@@ -23,6 +11,15 @@ setup(
     license='LICENSE.txt',
     description='Simple, flexible app for integrating static, unstructured '
                 'content in a Django site',
-    classifiers=filter(None, classifiers.split("\n")),
+    classifiers=[
+        'Topic :: Internet :: WWW/HTTP :: Dynamic Content',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Programming Language :: Python',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Development Status :: 4 - Beta',
+        'Operating System :: OS Independent',
+    ],
     long_description=open('README.rst').read(),
+    zip_safe=False, # because we're including media that Django needs
 )
