ac8779b7d59ebc095771f10592b1ffb6b2aeb415
Mark Lavin <mlavin@caktusgroup.com>
2012-10-26 15:10:02 -0400

    It's 2012 (almost 2013)

--- docs/conf.py
+++ docs/conf.py
@@ -41,7 +41,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = u'django-pagelets'
-copyright = u'2011, Caktus Consulting Group'
+copyright = u'2010-2012, Caktus Consulting Group'
 
 # The version info for the project you're documenting, acts as replacement for
 # |version| and |release|, also used in various other places throughout the
