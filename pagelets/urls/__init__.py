1d3ed34595867f811d72dcf1bddca8c888067a9c
Rebecca Lovewell <rebecca@caktusgroup.com>
2014-02-03 19:19:55 -0500

    Updated django.conf.urls import location

--- pagelets/urls/__init__.py
+++ pagelets/urls/__init__.py
@@ -1,4 +1,4 @@
-from django.conf.urls.defaults import *
+from django.conf.urls import include, patterns, url
 
 urlpatterns = patterns('',
     url(r'^management/', include('pagelets.urls.management')),
