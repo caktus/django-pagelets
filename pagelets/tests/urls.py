ab2ecac7f9af42c98c304d6d844d0c354bd8bb74
Calvin Spealman <calvin@caktusgroup.com>
2014-02-24 09:07:19 -0500

    Adding missing files in new tests package

---
+++ pagelets/tests/urls.py
@@ -0,0 +1,12 @@
+try:
+    # Django 1.4+
+    from django.conf.urls import include, patterns, url
+except ImportError:
+    # Django 1.3
+    from django.conf.urls.defaults import include, patterns, url
+from django.contrib import admin
+
+urlpatterns = patterns('',
+    url(r'^pagelets/', include('pagelets.urls')),
+    url(r'^admin/', include(admin.site.urls)),
+)
